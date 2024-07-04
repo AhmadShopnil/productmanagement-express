e7dc31ac895774e0156e588d33d8e0279e6b1d9d
AhmadShopnil <ahmadshopnil75@gmail.com>
2024-07-04 12:11:38 +0600

    create all services for product

---
+++ src/modules/product/product.services.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { TProduct } from './product.interface';
+import { Product } from './product.model';
+
+const createProductInDB = async (product: TProduct) => {
+  // Check if product already exists by name
+  const productExists = await Product.findOne({ name: product.name });
+
+  if (productExists) {
+    throw new Error('Product Already Exists!');
+  }
+
+  const result = await Product.create(product);
+  return result;
+};
+
+const getAllProductsFromDB = async () => {
+  const result = await Product.find();
+  return result;
+};
+
+const getSingleProductFromDB = async (productId: string) => {
+  const result = await Product.findById({ _id: productId });
+  return result;
+};
+
+const updateSingleProductInDB = async (
+  productId: string,
+  updateData: Partial<TProduct>,
+) => {
+  const productExists = await Product.findById(productId);
+
+  if (productExists) {
+    const result = await Product.findByIdAndUpdate(productId, updateData, {
+      new: true,
+    });
+    return result;
+  } else {
+    throw new Error('Product not found');
+  }
+};
+
+const deleteSingleProductInDB = async (productId: string) => {
+  const productExists = await Product.findById({ _id: productId });
+
+  if (productExists) {
+    const result = await Product.findByIdAndDelete({ _id: productId });
+    return result;
+  } else {
+    throw new Error('Product not found');
+  }
+};
+
+export const productService = {
+  createProductInDB,
+  getAllProductsFromDB,
+  getSingleProductFromDB,
+  updateSingleProductInDB,
+  deleteSingleProductInDB,
+};
