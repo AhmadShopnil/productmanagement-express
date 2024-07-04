4b1542849a4437bda5fc185ec6fb4ba37500350f
AhmadShopnil <ahmadshopnil75@gmail.com>
2024-07-04 12:17:57 +0600

    create controller functions

---
+++ src/modules/product/product.controller.ts
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import { productService } from './product.services';
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+
+    // Validate the product data using the validation schema
+    // const validatedData = productValidationSchema.parse(productData);
+
+    // Call the service to create the product in the database
+    const result = await productService.createProductInDB(productData);
+    res.status(200).json({
+      success: true,
+      message: 'Product successfully created',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to create new product',
+      error: {
+        code: 500,
+        description: error?.message,
+      },
+    });
+  }
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await productService.getAllProductsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to get all products',
+      error: {
+        code: 500,
+        description: error?.message,
+      },
+    });
+  }
+};
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const result = await productService.getSingleProductFromDB(productId);
+
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: 'Product fetched successfully',
+        data: result,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'Product not found',
+        error: {
+          code: 404,
+          description: 'Product not found!',
+        },
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch the product',
+      error: {
+        code: 500,
+        description: error?.message,
+      },
+    });
+  }
+};
+
+const updateSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const updateData = req.body;
+
+    const result = await productService.updateSingleProductInDB(
+      productId,
+      updateData,
+    );
+
+    res.status(200).json({
+      success: true,
+      message: 'Product successfully updated',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to update the product',
+      error: {
+        code: 500,
+        description: error?.message,
+      },
+    });
+  }
+};
+
+const deleteSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const result = await productService.deleteSingleProductInDB(productId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Product successfully deleted',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to delete the product',
+      error: {
+        code: 500,
+        description: error?.message,
+      },
+    });
+  }
+};
+
+export const productController = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateSingleProduct,
+  deleteSingleProduct,
+};
